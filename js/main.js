9252b161ff58a54ff9bc6a5a0fe596d162bd47f2
Henry Lung <henrylung@fb.com>
2017-08-16 02:05:06 -0700

    Site updated: 2017-08-16 02:05:05

---
+++ js/main.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+  // img_unveil
+  $("._img-container").unveil(0, function() {
+    let lg = this.getAttribute('data-large');
+    let sm = this.getAttribute('data-small');
+    let ph = this.querySelector('._placeholder');
+
+    let img_sm = new Image();
+    img_sm.src = sm;
+    img_sm.onload = function () {
+      if (ph) {
+        ph.style.paddingBottom = (this.height / this.width * 100) + '%';
+      }
+      img_sm.classList.add('loaded', 'img-responsive', '_img-blur');
+    };
+    this.append(img_sm);
+
+    let img_lg = new Image();
+    img_lg.src = lg;
+    img_lg.onload = function () {
+      if (ph) {
+        ph.style.paddingBottom = (this.height / this.width * 100) + '%';
+      }
+      img_lg.classList.add('loaded', 'img-responsive', '_img-final');
+      img_sm.classList.remove('loaded');
+    }
+    this.append(img_lg);
+  });
+});
